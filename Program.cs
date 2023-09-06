5575d205c21df57a9be86a8805706d147d8bb289
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 19:07:48 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//// See https://aka.ms/new-console-template for more information
+public class Program
+{
+    static void Main(string[] args)
+    {
+        //Vowel.Vowels();
+        //LeapYear.Leapyear();
+        //FlipCoin.Toss();
+        //Powerof2.PowerOf2();
+        //SwapNumbers.Swapnumbers();  
+        // EvenOrOdd.Even_Odd();
+        // Largest_Number.LargestNumber();
+        //QuoAndRem.Quo_Rem();
+        PrimeFactors.Factors();
+
+    }
+
+}
