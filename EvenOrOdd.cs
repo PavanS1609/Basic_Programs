b3b1995caba22e1ca3aac2a3e8264f0e75420aed
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 19:02:25 +0530

    Add files via upload

---
+++ EvenOrOdd.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class EvenOrOdd
+{
+    public static void Even_Odd()
+    {
+        Console.WriteLine("Enter the Number");
+        int Number = Convert.ToInt32(Console.ReadLine());
+        if (Number % 2 == 0)
+        {
+            Console.WriteLine("EvenNumber");
+        }
+        else
+        {
+            Console.WriteLine("OddNumber");
+        }
+
+
+    }
+}
