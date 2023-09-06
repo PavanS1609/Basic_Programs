40cfe5913e64447d33fbc2e6a9f15c0ea7345af8
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 19:05:49 +0530

    Add files via upload

---
+++ Powerof2.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Powerof2
+{
+   public static void PowerOf2()
+    {
+        for (int N = 0; N < 31; N++)
+        {
+            double power = Math.Pow(2, N);
+            Console.WriteLine(power);
+        }
+
+    }
+        
+       
+}
