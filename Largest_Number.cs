c5d0c9bb6aa7a020b7bb68869240cb8267116080
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 19:04:09 +0530

    Add files via upload

---
+++ Largest_Number.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Largest_Number
+{
+    public static void LargestNumber()
+    {
+
+        Console.WriteLine("Enter three Numbers");
+
+        double num1 =Convert.ToDouble(Console.ReadLine());
+        double num2 =Convert.ToDouble(Console.ReadLine());
+        double num3 = Convert.ToDouble(Console.ReadLine());
+
+         double result = Math.Max(num1,Math.Max( num2, num3));
+
+        Console.WriteLine("Largest Number is:" + result);
+
+    }
+}
