4c1e9723a1bc5124e5fc749b34fc14635b4e70b8
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 19:06:46 +0530

    Add files via upload

---
+++ PrimeFactors.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class PrimeFactors
+{
+    public static void Factors()
+    {
+        int num =Convert.ToInt32(Console.ReadLine());
+        //int N=Convert.ToInt32(Console.ReadLine());
+        for (int i = 1; i < num; i++)
+        {
+            if(num % i == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+        
+
+    }
+    public static void prime_factors()
+    {
+        int num1 ;
+    }
+
+}
