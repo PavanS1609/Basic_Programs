9fee970fa604ca85c4e281ec753513988666cfaa
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 19:08:22 +0530

    Add files via upload

---
+++ Quotient_Remainder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class QuoAndRem
+{
+    public static void Quo_Rem()
+    {
+        int num1=Convert.ToInt32(Console.ReadLine());   
+        int num2=Convert.ToInt32(Console.ReadLine());
+         
+        int result =num1 % num2;
+        Console.WriteLine(result);
+    }
+}
