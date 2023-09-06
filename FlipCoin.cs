f31a81e2cc6dc2e28666baddcc836d92d7daff3e
PavanS1609 <142468149+PavanS1609@users.noreply.github.com>
2023-09-06 19:01:27 +0530

    Add files via upload

---
+++ FlipCoin.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class FlipCoin
+{
+    public static void Toss()
+    {
+        int heads = 1;
+        int tails = 0;
+        Random r = new Random();
+        int result = r.Next(0, 2);
+        System.Console.WriteLine(result);
+    }
+
+}
